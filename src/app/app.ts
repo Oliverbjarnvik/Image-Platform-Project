f952b321b618d4cde5fcff8ee30dc694c3643b3d
Fiskett <oliver@fehre.com>
2025-12-10 13:23:05 +0100

    2025-12-10 all kod

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {Navbar} from './components/navbar/navbar';
+import {DropdownMenu} from './components/dropdown-menu/dropdown-menu';
+import {Footer} from './components/footer/footer';
+import {Form} from './components/form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, Navbar, RouterOutlet, Footer, DropdownMenu, Form],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Image-Platform-Project');
 }
+
